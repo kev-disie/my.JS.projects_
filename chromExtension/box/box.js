acfdc565290c776017931efe257a66899ccf1c3d
kev-disie <disiekelvin@gmail.com>
2026-02-25 06:59:56 +0300

    Up before the Opps

---
+++ chromExtension/box/box.js
@@ -0,0 +1,17 @@
+let boxEl = document.getElementById("box-el")
+
+boxEl.addEventListener("click",function(){
+    console.log("I want to open this box when it's clicked")
+}
+
+)
+
+let bestPrice = 520 //const
+
+let discount = 120  //const
+
+let shippingCos = 12  //let
+
+let shippingTime = "5 - 12 days"
+
+
--- chromExtension/chrome.js
+++ chromExtension/chrome.js
@@ -1,5 +1,23 @@
-let inputEl = document.getElementById("input-el")
+let myLeads = [];
 
-function buttonClicked(){
-    console.log( "Button clicked");
-}
+const inputEl = document.getElementById("input-el")
+
+const inputBtn = document.getElementById("input-btn")
+
+let lead ="WWW.awsomeLead.com"
+
+
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(lead)
+    console.log(myLeads)
+
+    console.log("Button clicked from addEventlistener...")
+})
+// eventListener using a function
+// function buttonClicked(){
+//     console.log( "Button clicked");
+// }
+
+
+ 
