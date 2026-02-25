dd8d39a28ad015b214ee3d65b0bf98dc8ab5c5df
kev <kev@M4ST3RS.M4ST3RS>
2026-02-25 15:44:50 +0300

    Numbers Don't lie 😌

--- chromExtension/box/box.js
+++ chromExtension/box/box.js
@@ -14,4 +14,26 @@ let shippingCos = 12  //let
 
 let shippingTime = "5 - 12 days"
 
+// let pEl = document.getElementById("p-el")
 
+let contEl = document.getElementById("conatiner")
+
+contEl.innerHTML = "<button onclick = 'buy()'> Buy !! </button> "
+
+
+// my way of doing stuff huh
+
+// contEl.addEventListener("click",function(){
+   
+
+//  return contEl.innerHTML += "<p>Thank you for shoping with Us</p>"
+// }
+    
+// )
+
+
+//Another alternative      USE THE FUNCTION RATHER THSN 
+
+function buy(){
+ document.getElementById("p-el").innerHTML = "<p>Thank you for shoping with Us.</p>"
+}
--- chromExtension/chrome.js
+++ chromExtension/chrome.js
@@ -1,23 +1,36 @@
-let myLeads = [];
+let myLeads = ["Kassandra","Naomi","Marione","cutepoison","Nasseh"];
 
 const inputEl = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
 
-let lead ="WWW.awsomeLead.com"
+const ulEL = document.getElementById("ul-el") //defined and get the u-el id from the DOM != assignment
+
+
+
+// let lead = inputEl.value
 
 
 
 inputBtn.addEventListener("click",function(){
-    myLeads.push(lead)
+    // myLeads.push(lead)
+    // console.log(myLeads)
+    myLeads.push(inputEl.value)
     console.log(myLeads)
+
+
+
+    
 
     console.log("Button clicked from addEventlistener...")
 })
-// eventListener using a function
-// function buttonClicked(){
-//     console.log( "Button clicked");
-// }
+// write a for loop that logs out the items in the myLeads array
+
+for (let i = 0; i < myLeads.length ;i++){
+    // console.log(myLeads[i])
+
+    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+}
 
 
- 
